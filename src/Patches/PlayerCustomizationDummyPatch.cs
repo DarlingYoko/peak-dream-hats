3e63a3ecd5f9646bdfc989c0c56cb683ac75a964
Creta Park <creta5164@gmail.com>
2025-07-13 21:59:22 +0900

    style: PlayerCustomizationDummyPatch : Clean up SetPlayerHat method

--- src/Patches/PlayerCustomizationDummyPatch.cs
+++ src/Patches/PlayerCustomizationDummyPatch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 using Plugin = MoreCustomizations.MoreCustomizationsPlugin;
@@ -7,39 +8,59 @@ using Plugin = MoreCustomizations.MoreCustomizationsPlugin;
 namespace MoreCustomizations.Patches;
 
 public class PlayerCustomizationDummyPatch {
+    
     [HarmonyPatch(typeof(PlayerCustomizationDummy), "SetPlayerHat")]
     [HarmonyPrefix]
     private static void SetPlayerHat(PlayerCustomizationDummy __instance, int index) {
-        if (Character.localCharacter == null)
+        
+        Character localCharacter = Character.localCharacter;
+        
+        if (!localCharacter || !localCharacter.TryGetComponent<CharacterCustomization>(out var characterCustomization))
             return;
-
-        CharacterCustomization characterCustomization = Character.localCharacter.gameObject.GetComponent<CharacterCustomization>();
-        if (characterCustomization == null)
+        
+        Renderer[] actualPlayerHats = characterCustomization.refs.playerHats;
+        Renderer[] dummyPlayerHats  = __instance.refs.playerHats;
+        
+        //Check if character hats more than dummy hats
+        if (actualPlayerHats.Length == dummyPlayerHats.Length)
             return;
-
-        // check if character hats more than dummy hats
-        if (characterCustomization.refs.playerHats.Length > __instance.refs.playerHats.Length) {
-            Plugin.Logger.LogInfo($"{characterCustomization.refs.playerHats.Length}-{__instance.refs.playerHats.Length}");
-
-            var scrHatTransform = characterCustomization.transform.Find(CharacterCustomizationPatch.HAT_PATH);
-            var hatTransform = __instance.transform.Find(CharacterCustomizationPatch.HAT_PATH);
-            var hatRenderList = new List<Renderer>(__instance.refs.playerHats);
-            var layer = __instance.refs.playerHats[0].gameObject.layer;     // layer-[Character]
-
-            for (int i = __instance.refs.playerHats.Length; i < characterCustomization.refs.playerHats.Length; i++) {
-                GameObject srcHatInstance = characterCustomization.refs.playerHats[i].gameObject;
-                // get the top gameobject of hat 
-                while (srcHatInstance.transform.parent != scrHatTransform)
-                    srcHatInstance = srcHatInstance.transform.parent.gameObject;
-                // copy the top gameobject of hat 
-                GameObject hatInstance = Object.Instantiate(srcHatInstance, __instance.refs.playerHats[0].transform.parent, false);
-                // get Renderer and add it to list
-                Renderer hatInstanceRender = hatInstance.GetComponentInChildren<Renderer>(true);
-                hatInstanceRender.gameObject.layer = layer;
-                hatRenderList.Add(hatInstanceRender);
-            }
-
-            __instance.refs.playerHats = hatRenderList.ToArray();
+        
+        Renderer firstDummyHatRenderer = dummyPlayerHats.FirstOrDefault();
+        
+        if (!firstDummyHatRenderer) {
+            
+            Plugin.Logger.LogError($"Something went wrong in {nameof(PlayerCustomizationDummyPatch)}...");
+            return;
         }
+        
+        int dummyHatLayer = firstDummyHatRenderer.gameObject.layer;     //layer-[Character]
+        
+        Transform actualHatTransform = characterCustomization.transform.Find(CharacterCustomizationPatch.HAT_PATH);
+        Transform dummyHatTransform  = __instance.transform.Find(CharacterCustomizationPatch.HAT_PATH);
+        
+        var hatRenderList = new List<Renderer>(dummyPlayerHats);
+        
+        //NOTE: Since this instantiates hats are cloning on just in time, it may causes main thread lock until it finished.
+        //    : But it doesn't seem like we're at a point where we need to consider it that way yet.
+        for (int hatIndex = dummyPlayerHats.Length; hatIndex < actualPlayerHats.Length; hatIndex++) {
+            
+            Transform actualHatInstance = actualPlayerHats[hatIndex].transform;
+            
+            //Find the top gameObject of hat instance.
+            while (actualHatInstance.parent != actualHatTransform)
+                actualHatInstance = actualHatInstance.parent;
+            
+            //Clone the top gameObject of hat instance.
+            GameObject clonedHatInstance = Object.Instantiate(actualHatInstance.gameObject, dummyHatTransform, false);
+            clonedHatInstance.name = actualHatInstance.name;
+            
+            //Get Renderer and add it to list
+            Renderer hatInstanceRender = clonedHatInstance.GetComponentInChildren<Renderer>(true);
+            
+            hatInstanceRender.gameObject.layer = dummyHatLayer;
+            hatRenderList.Add(hatInstanceRender);
+        }
+
+        __instance.refs.playerHats = hatRenderList.ToArray();
     }
 }
